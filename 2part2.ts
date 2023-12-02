593dbcbd42df9d0f78201a8d2d327b81652b9620
Mattias Knutsson <matknu.kth@gmail.com>
2023-12-02 16:31:31 +0100

    Day2part2

--- 2part2.ts
+++ 2part2.ts
@@ -3,15 +3,12 @@
 // Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 // Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 // Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
-// console.log('input', input)
 const fs = require("fs");
 const input = fs.readFileSync("input2.txt").toString()
 const rounds = input.split("\n");
 
-const isImpossible = ({ red, green, blue }) => red > 12 || green > 13 || blue > 14
 const parseSet = str => {
     const parts = str.split(",")
-    // console.log('parts', parts);
     const set = { red: 0, green: 0, blue: 0 }
     for (let i = 0; i < parts.length; i++) {
         const [count, type] = parts[i].trim().split(" ");
@@ -20,17 +17,26 @@ const parseSet = str => {
     return set
 }
 
-const isRowPossible = row => {
+const getFewestCube = row => {
     const [gameId, gamesStr] = row.split(":")
     const games = gamesStr.split(";").map(parseSet)
-    const impossible = games.some(isImpossible)
-    return !impossible
+    return games.reduce((acc, game) => {
+        acc.red = Math.max(acc.red, game.red)
+        acc.green = Math.max(acc.green, game.green)
+        acc.blue = Math.max(acc.blue, game.blue)
+        return acc
+    }, {
+        red: 0,
+        green: 0,
+        blue: 0
+    })
 }
-const score = rounds.reduce((acc, row, index) => {
-    let roundScore = 0;
-    if (isRowPossible(row)) {
-        roundScore = index + 1;
-    }
+
+const score = rounds.reduce((acc, round) => {
+    const { green, red, blue } = getFewestCube(round)
+    const roundScore = green * red * blue
+    console.log('roundScore', roundScore);
     return acc + roundScore
+
 }, 0)
-console.log('score', score);
+console.log('scorae', score);
